2f07f8f3499d8fa758e64dbe39264d0dbbbc03b9
HRx88 <S10262513@connect.np.edu.sg>
2024-01-15 23:21:41 +0800

    fixed Login.js code the fetch code and update s.js code  remove code that not in use

---
+++ Login.js
@@ -0,0 +1,49 @@
+const APIKEY = "65996a870b08685ca8232bf2";
+document.addEventListener("DOMContentLoaded", function () {
+  // [STEP 1]: Create our submit form listener
+  document
+    .querySelector(".btn-primary")
+    .addEventListener("click", function (e) {
+      // Prevent default action of the button
+      e.preventDefault();
+
+      // [STEP 2]: Call your function to fetch and process data
+      getdata();
+    });
+
+  function getdata() {
+    let settings = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "x-apikey": APIKEY,
+        "Cache-Control": "no-cache",
+      },
+    };
+    fetch("https://fed23-25a3.restdb.io/rest/accc", settings)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        let name = document.getElementById("exampleInputEmail1").value;
+        let password = document.getElementById("exampleInputPassword1").value;
+
+        for (let i = 0; i < data.length; i++) {
+          if (data[i].name === name && data[i].password === password) {
+            window.location.href = "index1.html";
+            return; // exit the loop if the user is found
+          }
+        }
+
+        // If the loop completes without redirecting, the credentials are incorrect
+        console.error("Invalid credentials");
+      })
+      .catch((error) => {
+        console.error("Fetch error:", error);
+      });
+  }
+});
